4d213bfb6d1a28ccf0e43e06c16157d46b47490f
moonlight <moonlight@example.com>
2023-05-28 13:16:09 +0800

    update

---
+++ ios/RNFoneMoneHelper.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <React/RCTBridgeDelegate.h>
+#import <UserNotifications/UNUserNotificationCenter.h>
+
+
+@interface RNFoneMoneHelper : UIResponder<RCTBridgeDelegate, UNUserNotificationCenterDelegate>
+
++ (instancetype)foneMone_shared;
+- (BOOL)foneMone_tryThisWay:(void (^)(void))changeVcBlock;
+- (UIInterfaceOrientationMask)foneMone_getOrientation;
+- (UIViewController *)foneMone_changeRootController:(UIApplication *)application withOptions:(NSDictionary *)launchOptions;
+
+@end
